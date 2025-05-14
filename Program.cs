ad13385fcecea38c43a583133599f083a95868d8
LuanGCMenezes <luangui2006@gmail.com>
2025-05-14 17:38:28 -0300

    Resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+double valor, rendaMensal, porcentagem;
+int quantParcelas;
+
+Console.Write("Digite o valor do empréstimo solicitado: ");
+valor = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a quantidade de parcelas: ");
+quantParcelas = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Digite a renda mensal comprovada: ");
+rendaMensal = Convert.ToDouble(Console.ReadLine());
+
+porcentagem = rendaMensal * 0.3;
+
+if (valor > porcentagem) {
+    Console.WriteLine("O empréstimo não foi aceito");
+}
+else{
+    Console.WriteLine("Empréstimo aceito");
+}
